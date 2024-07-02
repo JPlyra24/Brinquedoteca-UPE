ab5833e900d0fbc961a72411a30dfe0d2359d39e
João Lyra <joaolyra24@gmail.com>
2024-07-02 00:13:15 -0300

    Update tela login

--- brinquedoteca-upe/src/pages/Login.jsx
+++ brinquedoteca-upe/src/pages/Login.jsx
@@ -4,6 +4,9 @@ import LoginInput from '../components/LoginInput';
 import brinquedotecaLogo from '../assets/Brinquedoteca.png';
 import upeLogo from '../assets/Logoupe.png';
 import { request } from '../services/api';
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/800.css";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -26,29 +29,42 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <div className="login-header">
-        <img src={brinquedotecaLogo} alt="Brinquedoteca" className="logo" />
+      <div className='logo-header'>
+        <img src={brinquedotecaLogo} alt="Brinquedoteca" className='login-header'/>
       </div>
       <div className="login-form">
-        <img src={upeLogo} alt="UPE" className="upe-logo" />
-        <h2>Login</h2>
+        <div className='title'>
+          <h2>Login</h2>
+          <img src={upeLogo} alt="UPE" className="upe-logo" />
+        </div>
         {error && <p className="error-text">{error}</p>}
-        <LoginInput
-          type="email"
-          placeholder="Digite seu e-mail..."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <LoginInput
-          type="password"
-          placeholder="Digite sua senha..."
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="login-button" onClick={handleLogin}>Entrar</button>
-        <p className="signup-text">
-          Não possui conta? <a href="/signup">Cadastre-se</a>
-        </p>
+        <div className='input-container'>
+          <p className='login-title'>E-mail:</p>
+          <LoginInput
+            type="email"
+            placeholder="Digite seu e-mail..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p className='login-title'>Senha:</p>
+          <LoginInput
+            type="password"
+            placeholder="Digite sua senha..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='center'>
+          <div className="signup-container-horizontal">
+            <p className="signup-text">
+              Não possui conta? 
+              <Link to="/register">Cadastre-se</Link>
+            </p>
+            <Link to="/homeResponsable">
+              <button className="login-button" onClick={handleLogin}>Entrar</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );

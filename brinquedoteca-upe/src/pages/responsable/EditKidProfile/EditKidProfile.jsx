33b8fae76eab5168c066fbf315c17f738b475c14
João Lyra <joaolyra24@gmail.com>
2024-07-08 23:14:00 -0300

    Update EditKidProfile

--- brinquedoteca-upe/src/pages/responsable/EditKidProfile/EditKidProfile.jsx
+++ brinquedoteca-upe/src/pages/responsable/EditKidProfile/EditKidProfile.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './EditKidProfile.css'; 
+import TopBar from '../../../components/TopBar';
+import TextInput from '../../../components/TextInput';
+import { FaPen } from 'react-icons/fa';
 
 const EditKidProfile = () => {
   const [formData, setFormData] = useState({
@@ -38,46 +41,60 @@ const EditKidProfile = () => {
   };
 
   return (
-    <div className="profile-form-container">
-      <h2>Perfil da Criança</h2>
+    <div className='t7-container'>
+      <TopBar/>
+      <div className='t7-title-box'>
+        <h2 className='t7-title'>Perfil da Criança</h2>
+        <div className='t7-icon-box'>
+          <FaPen className='t7-icon'/>
+        </div>
+      </div>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Nome completo:</label>
-          <input
+      <div className="t4-form-group">
+          <p className='t4-label'>Nome completo:</p>
+          <TextInput
             type="text"
+            placeholder="Digite o nome completo..."
             name="nomeCompleto"
             value={formData.nomeCompleto}
             onChange={handleChange}
           />
         </div>
-        <div className="form-group">
-          <label>Data de nascimento:</label>
-          <input
-            type="date"
+        <div className="t4-form-group">
+          <p className='t4-label'>Data de nascimento:</p>
+          <TextInput
+            type="text"
+            placeholder="DD/MM/AAAA"
             name="dataNascimento"
             value={formData.dataNascimento}
             onChange={handleChange}
           />
         </div>
-        <div className="form-group">
-          <label>Observações:</label>
-          <textarea
+        <div className="t4-form-group">
+          <p className='t4-label'>Observações:</p>
+          <TextInput
+            type="text"
+            placeholder="Alergias, medos, etc..."
             name="observacoes"
             value={formData.observacoes}
             onChange={handleChange}
           />
         </div>
-        <div className="form-group">
-          <label>Descrição:</label>
-          <textarea
+        <div className="t4-form-group">
+          <p className='t4-label'>Descrição:</p>
+          <TextInput
+            type="text"
+            placeholder="Descrição..."
             name="descricao"
             value={formData.descricao}
             onChange={handleChange}
           />
         </div>
-        <div className="form-actions">
-          <button type="submit">Salvar</button>
-          <a href="/home">Voltar para Home?</a>
+        <div className='t7-buttons-box'>
+          <p>
+            <a href="/homeResponsable" className='t7-a'>Voltar para Home?</a>
+          </p>
+          <button type="submit" className='t7-button'>Salvar</button>
         </div>
       </form>
     </div>

2f3c68fcbb8ebf4edd7d61a8de8133a66305b6b8
João Lyra <joaolyra24@gmail.com>
2024-07-08 22:58:35 -0300

    Update RegisterKid

--- brinquedoteca-upe/src/pages/responsable/RegisterKid/RegisterKid.jsx
+++ brinquedoteca-upe/src/pages/responsable/RegisterKid/RegisterKid.jsx
@@ -4,6 +4,7 @@ import TextInput from '../../../components/TextInput';
 import brinquedotecaLogo from '../../../assets/Brinquedoteca.png';
 import upeLogo from '../../../assets/Logoupe.png';
 import { request } from '../../../services/api';
+import TopBar from '../../../components/TopBar';
 
 const RegisterKid = () => {
   const [formData, setFormData] = useState({
@@ -37,46 +38,58 @@ const RegisterKid = () => {
   };
 
   return (
-    <div className="register-child-container">
-      <div className="register-child-header">
-        <img src={brinquedotecaLogo} alt="Brinquedoteca" className="logo" />
-        <img src={upeLogo} alt="UPE" className="upe-logo" />
+    <div className="t4-container">
+      <TopBar/>
+      <div className='t4-title-box'>
+        <h2 className='t4-title'>Cadastrar criança</h2>
       </div>
-      <div className="register-child-form">
-        <h2>Cadastrar criança</h2>
-        {error && <p className="error-text">{error}</p>}
-        <TextInput
-          type="text"
-          placeholder="Digite o nome completo..."
-          name="nomeCompleto"
-          value={formData.nomeCompleto}
-          onChange={handleChange}
-        />
-        <TextInput
-          type="text"
-          placeholder="DD/MM/AAAA"
-          name="dataNascimento"
-          value={formData.dataNascimento}
-          onChange={handleChange}
-        />
-        <TextInput
-          type="text"
-          placeholder="Alergias, medos, etc..."
-          name="observacoes"
-          value={formData.observacoes}
-          onChange={handleChange}
-        />
-        <TextInput
-          type="text"
-          placeholder="Descrição..."
-          name="descricao"
-          value={formData.descricao}
-          onChange={handleChange}
-        />
-        <button className="register-child-button" onClick={handleRegisterChild}>Cadastrar</button>
-        <p className="home-text">
-          Voltar para Home? <a href="/home">Voltar</a>
-        </p>
+      <div>
+        <div className="t4-form-group">
+          <p className='t4-label'>Nome completo:</p>
+          <TextInput
+            type="text"
+            placeholder="Digite o nome completo..."
+            name="nomeCompleto"
+            value={formData.nomeCompleto}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="t4-form-group">
+          <p className='t4-label'>Data de nascimento:</p>
+          <TextInput
+            type="text"
+            placeholder="DD/MM/AAAA"
+            name="dataNascimento"
+            value={formData.dataNascimento}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="t4-form-group">
+          <p className='t4-label'>Observações:</p>
+          <TextInput
+            type="text"
+            placeholder="Alergias, medos, etc..."
+            name="observacoes"
+            value={formData.observacoes}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="t4-form-group">
+          <p className='t4-label'>Descrição:</p>
+          <TextInput
+            type="text"
+            placeholder="Descrição..."
+            name="descricao"
+            value={formData.descricao}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='t4-buttons-box'>
+          <p>
+            <a href="/homeResponsable" className='t4-a'>Voltar para Home?</a>
+          </p>
+          <button className='t4-button' onClick={handleRegisterChild}>Cadastrar</button>
+        </div>
       </div>
     </div>
   );

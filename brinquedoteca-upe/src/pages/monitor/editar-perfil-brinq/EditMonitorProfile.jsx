9c18587485a35cf84dab55b3e3e1f5dca93b8d41
João Lyra <joaolyra24@gmail.com>
2024-07-11 22:15:07 -0300

    Update EditMonitorProfile

--- brinquedoteca-upe/src/pages/monitor/editar-perfil-brinq/EditMonitorProfile.jsx
+++ brinquedoteca-upe/src/pages/monitor/editar-perfil-brinq/EditMonitorProfile.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import TopBar from '../../../components/TopBar';
+import { FaPen } from 'react-icons/fa';
+import './EditMonitorProfile.css'; 
+import TextInput from '../../../components/TextInput';
 
 const EditMonitorProfile = () => {
   const [formData, setFormData] = useState({
@@ -26,45 +30,115 @@ const EditMonitorProfile = () => {
   };
 
   return (
-    <div className="perfil-container">
-      <h2>Perfil do Brinquedista</h2>
-      <form>
-        <label>Nome completo:</label>
-        <input type="text" name="cep" value={formData.cep} onChange={handleChange} />
-
-        <label>CEP:</label>
-        <input type="text" name="nome" value={formData.nome} onChange={handleChange} />
-
-        <label>Rua:</label>
-        <input type="text" name="rua" value={formData.rua} onChange={handleChange} />
-
-        <div className="form-row">
-          <div>
-            <label>Número:</label>
-            <input type="text" name="numero" value={formData.numero} onChange={handleChange} />
-          </div>
-          <div>
-            <label>Bairro:</label>
-            <input type="text" name="bairro" value={formData.bairro} onChange={handleChange} />
-          </div>
+    <div className='t10-container'>
+      <TopBar/>
+      <div className='t10-title-box'>
+        <h2 className='t10-title'>Perfil do brinquedista</h2>
+        <div className='t10-icon-box'>
+          <FaPen className='t10-icon'/>
         </div>
-
-        <label>E-mail:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-
-        <label>Telefone para contato:</label>
-        <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} />
-
-        <label>CPF:</label>
-        <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} />
-
-        <label>Curso:</label>
-        <input type="text" name="curso" value={formData.curso} onChange={handleChange} />
-
-        <button type="button" onClick={handleSave}>Salvar</button>
+      </div>
+      <form>
+        <div className="t10-form-group">
+          <p className='t10-label'>Nome completo:</p>
+          <TextInput
+            type="text"
+            name="name"
+            placeholder="Nome completo"
+            value={formData.cep}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="t10-form-group">
+          <p className='t10-label'>CEP:</p>
+          <TextInput
+            type="text" 
+            name="CEP"
+            placeholder="Nome completo"
+            value={formData.nome}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="t10-form-group">
+          <p className='t10-label'>Rua:</p>
+          <TextInput
+            type="text"
+            name="rua"
+            placeholder="Rua"
+            value={formData.rua}
+            onChange={handleChange}
+          />
+        </div>
+          <div className='t6-div-bairro-resp'>
+          <div className="t10-form-group">
+              <p className='t10-label'>Número:</p>
+              <TextInput
+                type="text"
+                name="numero"
+                placeholder="Número"
+                value={formData.numero}
+                onChange={handleChange} 
+              />
+            </div>
+            <div className="t10-form-group">
+              <p className='t10-label'>Bairro:</p>
+              <TextInput
+                type="text"
+                name="bairro"
+                placeholder="Bairro"
+                value={formData.bairro}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="t10-form-group">
+            <p className='t10-label'>E-mail:</p>
+            <TextInput
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="t10-form-group">
+            <p className='t10-label'>Telefone para contato:</p>
+            <TextInput
+              type="text"
+              name="telefone"
+              placeholder="Telefone para contato"
+              value={formData.telefone}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="t10-form-group">
+            <p className='t10-label'>CPF:</p>
+            <TextInput
+              type="text"
+              name="cpf"
+              placeholder="CPF"
+              value={formData.cpf}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="t10-form-group">
+            <p className='t10-label'>Curso:</p>
+            <TextInput
+              type="text"
+              name="curso"
+              placeholder="Curso"
+              value={formData.curso}
+              onChange={handleChange}
+            />
+          </div>
+          
       </form>
-
-      <a href="/homemonitor">Voltar para Home?</a>
+      <div className='t10-end-resp-prof'>
+        <div className="t10-back-link">
+          <a href="/homeResponsable" className='t10-a'>Voltar para Home?</a>
+        </div>
+        <button type="submit" className='t10-button'>Salvar</button>
+      </div>
     </div>
   );
 };

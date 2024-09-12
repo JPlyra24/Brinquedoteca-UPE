669e29982aeac681ea48cc5944d37d3ca698f8c3
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-09-12 15:11:33 -0300

    b

---
+++ brinquedoteca-upe/src/pages/monitor/EditMonitorProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditMonitorProfile = () => {
+  return (
+    <div>
+      <h1>EditMonitorProfile</h1>
+    </div>
+  );
+};
+
+export default EditMonitorProfile;
---
+++ brinquedoteca-upe/src/pages/monitor/KidProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KidProfile = () => {
+  return (
+    <div>
+      <h1>KidProfile</h1>
+    </div>
+  );
+};
+
+export default KidProfile;
